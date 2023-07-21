b019a0e1e7b56c250c65ebdffa045de472e36539
Melvin <meco0597@gmail.com>
2023-07-20 18:55:25 -0700

    Made loader a bit better

--- ui/src/components/Loader.js
+++ ui/src/components/Loader.js
@@ -4,6 +4,7 @@ import { Card, CardContent, Typography, LinearProgress } from '@mui/material';
 const Loader = () => {
   const [progress, setProgress] = useState(0);
   const [headerIndex, setHeaderIndex] = useState(0);
+  const [showHighDemand, setShowHighDemand] = useState(false);
   const headers = [
     "Asking the AI model for the best gift ideas...",
     "Finding unique and thoughtful gift suggestions...",
@@ -17,12 +18,8 @@ const Loader = () => {
   ];
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
-  useEffect(() => {
     const interval = setInterval(() => {
-      setProgress((prevProgress) => prevProgress + Math.random() * 15);
+      setProgress((prevProgress) => Math.min(prevProgress + Math.random() * 15, 90));
     }, 1000);
 
     return () => {
@@ -40,12 +37,25 @@ const Loader = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Replace this with the logic to handle what happens if the page takes longer than 10 seconds to load.
+      // For demonstration purposes, I'm just logging a message here.
+      setShowHighDemand(true);
+      console.log("Page load is taking longer than 10 seconds!");
+    }, 13000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div className="loader-container">
       <Card sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', minWidth: '70vw', maxWidth: '90vw', borderColor: 'rgb(100, 100, 100)' }}>
         <CardContent>
-          <Typography sx={{ animation: '$fade-in 1s linear infinite' }} variant="h7" color={'rgb(80, 80, 80)'} component="div" gutterBottom>
-            {headers[headerIndex]}
+          <Typography sx={{ animation: '$fade-in 1s linear infinite' }} variant="h7" color={'rbg(60, 60, 60)'} component="div" gutterBottom>
+            {showHighDemand ? 'We are experiencing high demand at this moment. Please bear with us, awesome gift suggestions are incoming!' : headers[headerIndex]}
           </Typography>
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <div style={{ flex: 4 }}>
@@ -56,7 +66,7 @@ const Loader = () => {
                   marginTop: 2,
                   '& .MuiLinearProgress-bar': {
                     borderRadius: 5,
-                    backgroundImage: `linear-gradient(90deg, rgba(148,98,250,1) ${100 - progress}%, rgba(0,77,252,1) 68%)`
+                    backgroundImage: `linear-gradient(90deg, rgba(148,98,250,1) ${Math.min(100 - progress, 90)}%, rgba(0,77,252,1) 68%)`
                   }
                 }}
                 variant="determinate"
